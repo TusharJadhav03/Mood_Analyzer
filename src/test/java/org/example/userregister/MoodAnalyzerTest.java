82d06251c4f68e005b85ad409000b94555930b8c
Tushar <tushar.jadhav.work@gmail.com>
2024-04-22 22:47:56 +0530

    TC 1.2 Return HAPPY message

--- src/test/java/org/example/userregister/MoodAnalyzerTest.java
+++ src/test/java/org/example/userregister/MoodAnalyzerTest.java
@@ -11,7 +11,12 @@ class MoodAnalyzerTest {
     void analyzeMood() throws Exception{
         String mood = moodAnalyzer.analyzeMood("I am in Sad Mood".toLowerCase());
         Assertions.assertEquals(mood,"SAD");
+    }
 
+    @Test
+    void analyzeHappyMood() throws  Exception{
+        String mood = moodAnalyzer.analyzeMood("I am in Any mood".toLowerCase());
+        Assertions.assertEquals(mood,"HAPPY");
     }
 
 
